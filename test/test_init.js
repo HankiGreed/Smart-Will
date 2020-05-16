ba0f37b683fd8dab69c317b0d97c0f1c909ec702
Kiran Hegde <kiranhegde619@gmail.com>
2020-05-16 16:37:07 +0530

    :cold_sweat:Tests covering current functions

--- test/test_init.js
+++ test/test_init.js
@@ -1,4 +1,4 @@
-const Will = artifacts.require('Will');
+const Will = artifacts.require('TestWill');
 
 contract('Will', (accounts) => {
   let will;
@@ -7,61 +7,82 @@ contract('Will', (accounts) => {
   //const NonExistent = 0;
   const Created = 1;
   const Active = 2;
-  //const OverDeadline = 3;
-  //const PaidOut = 4;
+  const PaidOut = 3;
 
   beforeEach(async () => {
     will = await Will.new({from: accounts[0], gas: 2000000});
-    await will.initWill({value: ONE_ETH, from: accounts[0]});
+    await will.initWill({value: 3 * ONE_ETH, from: accounts[0]});
   });
   it('Should have the right amount and state', async () => {
     let amount = await will.getTotalAmount({from: accounts[0]});
-    let etherAmount = web3.utils.toBN('1000000000000000000');
+    let etherAmount = web3.utils.toBN('5000000000000000000');
     expect(amount).to.deep.equal(etherAmount);
     let state = await will.getWillState({from: accounts[0]});
     expect(state.valueOf().toNumber()).to.equal(Created);
   });
   it('Should Initialize will and store correct details', async () => {
+    let etherAmount = web3.utils.toBN('1000000000000000000');
     await will.editWill(
       [accounts[1], accounts[2], accounts[3]],
-      [3000000000000, 3000000000000, 4000000000000],
+      [etherAmount, etherAmount, etherAmount],
       599,
       {from: accounts[0]},
     );
 
     let beneficiaries = await will.getAllBeneficiaries({from: accounts[0]});
     let shares = await will.getSharesOfBeneficiaries({from: accounts[0]});
-    expect(shares[0].toNumber()).to.equal(3000000000000);
-    expect(shares[1].toNumber()).to.equal(3000000000000);
-    expect(shares[2].toNumber()).to.equal(4000000000000);
+    expect(shares[0]).to.deep.equal(etherAmount);
+    expect(shares[1]).to.deep.equal(etherAmount);
+    expect(shares[2]).to.deep.equal(etherAmount);
     expect(beneficiaries[0]).to.deep.equal(accounts[1]);
     expect(beneficiaries[1]).to.deep.equal(accounts[2]);
     expect(beneficiaries[2]).to.deep.equal(accounts[3]);
 
     let endDate = await will.getCurrentEndDate({from: accounts[0]});
-    expect(endDate.toNumber()).to.equal(599);
+    expect(Math.floor(endDate.toNumber() / 10)).to.equal(
+      Math.floor((Date.now() + 599000) / 10000),
+    );
     let state = await will.getWillState({from: accounts[0]});
     expect(state.valueOf().toNumber()).to.equal(Active);
   });
 
   it('Should let owner make modifications', async () => {
+    let etherAmount = web3.utils.toBN('1000000000000000000');
     await will.editWill(
       [accounts[1], accounts[3]],
-      [3000000000000, 3000000000000],
-      509,
+      [etherAmount, etherAmount],
+      899,
       {from: accounts[0]},
     );
 
     let beneficiaries = await will.getAllBeneficiaries({from: accounts[0]});
     let shares = await will.getSharesOfBeneficiaries({from: accounts[0]});
-    expect(shares[0].toNumber()).to.equal(3000000000000);
-    expect(shares[1].toNumber()).to.equal(3000000000000);
+    expect(shares[0]).to.deep.equal(etherAmount);
+    expect(shares[1]).to.deep.equal(etherAmount);
     expect(beneficiaries[0]).to.deep.equal(accounts[1]);
     expect(beneficiaries[1]).to.deep.equal(accounts[3]);
 
     let endDate = await will.getCurrentEndDate({from: accounts[0]});
-    expect(endDate.toNumber()).to.equal(509);
+    /*Added one second cus there's a gap between EVM's time and 
+        system time (System time is one more)*/
+    expect(Math.floor(endDate.toNumber() / 10)).to.equal(
+      Math.floor((Date.now() + 899000) / 10000),
+    );
     let state = await will.getWillState({from: accounts[0]});
     expect(state.valueOf().toNumber()).to.equal(Active);
+  });
+
+  it('Should payout wills after deadline', async () => {
+    let etherAmount = web3.utils.toBN('1000000000000000000');
+    await will.editWill(
+      [accounts[1], accounts[3]],
+      [etherAmount, etherAmount],
+      899,
+      {from: accounts[0]},
+    );
+    await will.setCurrentTime(910, {from: accounts[0]});
+    await will.payoutExpiredWills({from: accounts[0]});
+    let state = await will.getWillState({from: accounts[0]});
+    expect(state.valueOf().toNumber()).to.equal(PaidOut);
   });
 });
